fe8d37fe11a08bbb0da71a80b21e70ee1cba090b
rbnograles <ryannograles.025.gtsi@gmail.com>
2022-10-09 15:20:21 +0800

    fixed port redirection and added some main start up logs

--- src/main/java/com/example/demo/MainApp.java
+++ src/main/java/com/example/demo/MainApp.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainApp {
+public class MainApp implements CommandLineRunner {
+
+	private static final int PORT = 3000;
+	private static final Logger log = LoggerFactory.getLogger(MainApp.class.getName());
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApp.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Server starts at port " + PORT);
 	}
 }
--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@RequestMapping("/api/v1/person")
 @RestController
+@RequestMapping("/api")
 public class PersonController {
     private final PersonService personService;
     @Autowired
